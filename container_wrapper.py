4f82acb961ce59e5edf81f6f5b7c2f0ef8528cbe
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2022-07-31 16:27:49 -0300

    added setup command

--- container_wrapper.py
+++ container_wrapper.py
@@ -5,36 +5,54 @@ from kubernetes.client.models import V1EnvVar
 class ContainerWrapper:
     '''
     Wrapper for dsl.ContainerOp. Already manages file parsing through pipeline steps.
+
+    Attributes:
+        - name:
     '''
 
     MINIO_SCRIPT_URL = 'https://raw.githubusercontent.com/LombardiDaniel/marvin-rfc-poc/main/minio_utils.py'
-    SCRIPT_NAME = '__download_script_for_minio_in_container.py'
+    SCRIPT_NAME = '__script_for_minio_in_container.py'
 
     COMMAND = '/usr/local/bin/python -m pip install --upgrade pip'
     COMMAND += 'pip install minio && '
     COMMAND += f'curl {MINIO_SCRIPT_URL} -o {SCRIPT_NAME} && '
-    COMMAND += f'python {SCRIPT_NAME} '
+    COMMAND_ENDING += f' && python {SCRIPT_NAME} '
 
-    def __init__(self, name='name', image='python:3.7', file_inputs: list = [], file_outputs: list = [], verbose=False, *args, **kwargs):
+    @property
+    def setup_command(self):
+        '''
+        Setup command will run on every container (eg: 'pip install -r requirements.txt')
+        or wtvr else is specified by the user.
+        '''
+        setup_command = ContainerWrapper.COMMAND + setup_command + COMMAND_ENDING
+        return setup_command
+
+    def __init__(self,
+                 name='name',
+                 image='python:3.7',
+                 file_inputs=[],
+                 file_outputs=[],
+                 setup='pip install -r requirements.txt',
+                 verbose=False,
+                 *args,
+                 **kwargs
+                 ):
         self.file_inputs = file_inputs
         self.file_outputs = file_outputs
         self.image = image
         self.env_vars = kwargs
         self.name = name
         self.verbose = verbose
-
-    def push(self):
-        pass
-
-    def pop(self):
-        pass
+        self.setup = setup
 
     def run(self, script_to_wrap):
+        '''
+        '''
         download_command = ''
         upload_command = ''
 
         if self.file_inputs != []:
-            download_command = ContainerWrapper.COMMAND + 'download '
+            download_command = self.setup_command + 'download '
             for file in self.file_inputs:
                 download_command += f' {file} '
             download_command += '-v && ' if self.verbose else ' && '
